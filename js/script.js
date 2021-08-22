957af33f6f6ee5077f6b623e72ec6a7e6f363c45
Andrew-R-Law <andy.bob.law91@gmail.com>
2021-08-22 15:27:07 -0700

    Draft for 'Review My Project'

--- js/script.js
+++ js/script.js
@@ -2,20 +2,17 @@
 Treehouse Techdegree: Data Pagination and Filtering
 */
 
+/**
+ * Code Necessary for 'Meets Expectations.'
+ */
 
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab
-   Reach out in your Slack community if you have questions
-*/
+//Sets number of items to display to 9. (Not sure how to dynamically change this yet.)
 
 let numOfItems = 9;
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+
+//showPage function. Takes a list of students as the first parameter and which page the user is on as the second.
+//Constructs an li element with the student info and appends it inside the ul element of the source HTML.
 
 function showPage(list, page) {
    const startIndex = ( page * numOfItems ) - numOfItems;
@@ -43,10 +40,12 @@ function showPage(list, page) {
 }
 
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+//The addPagination function which accepts a list of students as a parameter.
+//Determines how many pages are needed, depending on the number of items to be displayed per page.
+//Adds a button for each page needed.
+//If there is at least one button, the first gets the class of 'active'.
+//Changes which button is active upon click.
+//Calls the showPage function with the page number clicked passed in as the second parameter.
 
 function addPagination (list) {
    const numOfPages = Math.ceil(list.length / numOfItems);
@@ -78,11 +77,15 @@ function addPagination (list) {
    });
 }
 
-// Call functions
+// Calls both functions
 showPage(data, 1);
 addPagination(data);
 
-//Code for Exceeds
+/*
+* Code necessary for 'Exceeds Expectations'.
+*/
+
+//Creates a search bar to be appended inside the header at the end.
 
 const searchBar = `
    <label for="search" class="student-search">
@@ -92,30 +95,45 @@ const searchBar = `
    </label>`;
 document.querySelector('.header').insertAdjacentHTML('beforeend', searchBar);
 
+//Variables that store the input of the user in the search bar, the search button, and which list of students match the user's input, respectively.
+
 const search = document.querySelector('#search');
 const submit = document.querySelector('.student-search button');
 let studentMatches = [];
 
+//The searchForStudents function which takes a search input and a list of names as parameters.
+//Resets the array of studentMatches to an empty array each time it is called.
+//Iterates through list of names, storing the first and last in a variable called 'studentName'
+//If the user input is included in the studentName, the name gets pushed to the studentMatches array.
+
 function searchForStudents (searchInput, names) {
    studentMatches = [];
    for (i = 0; i < names.length; i++){
-      if (names[i].name.first.toLowerCase().includes(searchInput.value.toLowerCase()) || names[i].name.last.toLowerCase().includes(searchInput.value.toLowerCase()) ) {
+      const studentName = names[i].name.first.toLowerCase() + ' ' + names[i].name.last.toLowerCase();
+      if (studentName.includes(searchInput.value.toLowerCase()) ) {
          studentMatches.push(names[i]);
       }
    }
 }
 
+
+//Event listener on the search button.
+//Calls the searchForStudents function to fill the studentMatches array.
+//Then calls the showPage and addPagination functions passing them the studentMatches array.
+//If there are no matches, changes the inner text of the <h2> element to 'No results found.' Otherwise, resets it to 'STUDENTS'.
 submit.addEventListener('click', () => {
    searchForStudents(search, data);
    showPage(studentMatches, 1);
    addPagination(studentMatches);
    const header = document.querySelector('h2');
    if (studentMatches.length === 0) {
-      header.innerText = 'No matches found.';
+      header.innerText = 'No results found.';
    } else {
       header.innerText = 'STUDENTS';
    }
 });
+
+//An event listener that is much the same as the previous, but listening for key strokes in the search bar instead.
 
 search.addEventListener('keyup', () => {
    searchForStudents(search, data);
@@ -123,7 +141,7 @@ search.addEventListener('keyup', () => {
    addPagination(studentMatches);
    const header = document.querySelector('h2');
    if (studentMatches.length === 0) {
-      header.innerText = 'No matches found.';
+      header.innerText = 'No results found.';
    } else {
       header.innerText = 'STUDENTS';
    }
